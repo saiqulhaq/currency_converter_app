e7c38b6df25c970a95001fec5c03fbabc4ea0496
M. Saiqul Haq <saiqulhaq@gmail.com>
2019-02-09 18:06:16 +0700

    feat(api): add Rate API controller

---
+++ app/controllers/api/rates_controller.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module Api
+  class RatesController < ::ApplicationController
+    def history
+      return render json: 'invalid date' if date_param.nil?
+
+      cache_key = "#{self.class}:historical:#{date_param}"
+      json = Rails.cache.fetch(cache_key) do
+        api.historical(date_param)
+      end
+      render json: json
+    end
+
+    def live
+      now = Time.zone.now
+      cache_key = "#{self.class}:live:#{now.to_date}:#{now.hour}"
+      json = Rails.cache.fetch(cache_key) do
+        api.live
+      end
+      render json: json
+    end
+
+    private
+
+    def api
+      @api ||= RateApi.new
+    end
+
+    def date_param
+      return @date_param if defined?(@date_param)
+      @date_param = begin
+                      Date.parse params.require(:date)
+                    rescue ArgumentError
+                      nil
+                    end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#root'
+
+  namespace :api do
+    resource :rates, only: [] do
+      collection do
+        get :history
+        get :live
+      end
+    end
+  end
 end
